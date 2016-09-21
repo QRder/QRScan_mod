4978ab1016dbe17b26cc04aa23156d9b5bbf9e4b
QRder <ro0ted@Ro0teDs-MacBook-Pro.local>
2016-09-22 00:42:10 +0200

    -

--- js/index.js
+++ js/index.js
@@ -13,10 +13,11 @@ function startScan() {
             var s = "Sie sitzen an  " + result.text + "<br>bitte nehmen Sie nun Ihre Bestellung vor";
             resultDiv.innerHTML = s;
         document.getElementById("startScan").value = "" + result.text ;
+        document.getElementById("startScan").id = "table" ;
             var elem = document.getElementById("buynow");
             var newtag = document.createElement("input");
             newtag.setAttribute("type","button");
-            newtag.setAttribute("onclick","window.open(" + result.text + ", '_system')");
+            newtag.setAttribute(".click(function()" + result.text + ", '_system')");
             newtag.setAttribute("id","startBuy");
             newtag.setAttribute("class","btn-primary");
             newtag.setAttribute("value","JETZT BESTELLEN");
